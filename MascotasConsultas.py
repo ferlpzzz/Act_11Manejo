af1754e51e55965316d812aab17da6c0042f182d
Jose Sulecio <josesulecio6@gmail.com>
2026-08-19 11:23:17 -0600

    Ya esta registrar mascotas

--- MascotasConsultas.py
+++ MascotasConsultas.py
@@ -11,3 +11,33 @@ def cargar_mascotas():
 def guardar_mascotas(mascotas):
     with open('mascotas.json', 'w') as archivo:
         json.dump(mascotas, archivo, indent=4)
+
+def registrar_mascota():
+    mascotas = cargar_mascotas()
+    
+    codigo = input("Ingrese el codigo de la mascota: ") #
+    
+    if codigo in mascotas:
+        print("Ya existe una mascota con ese codigo.")
+        return
+
+    nombre = input("Nombre: ") 
+    especie = input("Especie: ") 
+    raza = input("Raza: ")
+    fecha_nac = input("Fecha de nacimiento: ") 
+    propietario = input("Nombre del propietario: ") 
+    telefono = input("Telefono: ") 
+    estado = input("Estado (activo/inactivo): ").lower()
+
+    mascotas[codigo] = {
+        "Nombre": nombre,
+        "Especie": especie,
+        "Raza": raza,
+        "Fecha de nacimiento": fecha_nac,
+        "Nombre del propietario": propietario,
+        "Telefono": telefono,
+        "Estado": estado
+    }
+    
+    guardar_mascotas(mascotas)
+    print("Mascota guardada.")
